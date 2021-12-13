83cf9d810eaf66370b3f4c6c77be1bad87d8e13d
Jet Situ <situjet@berkeley.edu>
2021-12-13 13:04:59 -0800

    moving things into init

--- 149server/backend/control_test.py
+++ 149server/backend/control_test.py
@@ -23,7 +23,7 @@ async def main(address):
                 print("Type message and send with Enter key")
                 while True:
                     display = input("")
-                    await client.write_gatt_char(VOL_UUID, bytes(display, 'utf-8'))
+                    await client.write_gatt_char(START_UUID, bytes(display, 'utf-8'))
             except Exception as e:
                 print(f"\t{e}")
     except BleakError as e:
--- raspberryPiMidiPlaying/songbirdapp.py
+++ raspberryPiMidiPlaying/songbirdapp.py
@@ -86,9 +86,9 @@ def register_app_error_cb(error):
 
 class SongbirdService(Service):
     SONGBIRD_SVC_UUID = "314b2cb7-d379-474f-832f-6f833657e7e2"
-    songbird = mp.songbirdControl()
 
     def __init__(self, bus, index):
+        songbird = mp.songbirdControl()
         Service.__init__(self, bus, index, self.SONGBIRD_SVC_UUID, True)
         self.add_characteristic(StartCharacteristic(bus, 0, self))
         self.add_characteristic(StopCharacteristic(bus, 1, self))
