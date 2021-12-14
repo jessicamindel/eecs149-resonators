a9a13d4a518112378c327b33d6a4f2409005894b
Jet Situ <situjet@berkeley.edu>
2021-12-13 17:12:19 -0800

    what

--- raspberryPiMidiPlaying/midiplayer.c
+++ raspberryPiMidiPlaying/midiplayer.c
@@ -34,9 +34,9 @@ static int songbirdControl_init(PyObject *self, PyObject *soundfont, PyObject *m
     songbirdControl *new_songbird;
     new_songbird = malloc(sizeof(songbirdControl));
 
-    (*new_songbird).settings = new_fluid_settings();
-    (*new_songbird).synth = new_fluid_synth((*new_songbird).settings);
-    (*new_songbird).player = new_fluid_player((*new_songbird).synth);
+    // (*new_songbird).settings = new_fluid_settings();
+    // (*new_songbird).synth = new_fluid_synth((*new_songbird).settings);
+    // (*new_songbird).player = new_fluid_player((*new_songbird).synth);
     //fluid_settings_setstr((*new_songbird).settings, "audio.driver", "alsa");
     //fluid_player_set_playback_callback((*new_songbird).player, handle_tick, (*new_songbird).player)
     //(*new_songbird).adriver = new_fluid_audio_driver((*new_songbird).settings, (*new_songbird).synth);
