299d7d99cff81660bd7216e2cc00c757639b4cf7
Jet Situ <situjet@berkeley.edu>
2021-12-13 18:32:54 -0800

    not being detected

--- raspberryPiMidiPlaying/setup.py
+++ raspberryPiMidiPlaying/setup.py
@@ -7,7 +7,7 @@ def main():
     # Use the setup function we imported and set up the modules.
     # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
     # TODO: YOUR CODE HERE
-    module1 = Extension('songbird', sources = ['songbird.c'], extra_link_args=LDFLAGS)
+    module1 = Extension('songbird', sources = ['songbird.c'], extra_compile_args = CFLAGS, extra_link_args=LDFLAGS)
     setup(name = 'songbird', version = '1.0', description = 'This is the songbird Package', ext_modules = [module1])
 
 if __name__ == "__main__":
